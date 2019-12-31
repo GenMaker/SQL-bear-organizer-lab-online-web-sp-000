7b56c91d9ccdcb289ba23afcf005720351274eb9
Panela Leung <leungpanela@gmail.com>
2019-12-31 06:12:28 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,9 @@
-INSERT INTO bears ()
+INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES("Mr.Chocolate, 1, F, brown,");
+INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES();
+INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES();
+INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES();
+INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES();
+INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES();
+INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES();
+INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES();
+INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES();
