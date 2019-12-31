6955a422864a9d0d35aeb40e72253ad9aa1dbf7a
Panela Leung <leungpanela@gmail.com>
2019-12-31 06:17:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,8 @@
-INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES("Mr.Chocolate, 1, F, brown,");
-INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES();
-INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES();
-INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES();
-INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES();
-INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES();
-INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES();
-INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES();
-INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES();
+INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES("Mr.Chocolate", 1, "F", "brown", "calm", 1);
+INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES("Rowdy", 2, "F", "brown", "calm", 1);
+INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES("Tabitha", 1, "M", "black", "calm", 0);
+INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES("Sergeant Brown", 3, "F", "brown", "calm", 1);
+INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES("Melissa", 4, "M", "black", "calm", 0);
+INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES("Grinch", 1, "F", "brown", "calm", 1);
+INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES("Wendy", 2, "M", "black", "calm", 1);
+INSERT INTO bears (name,age,gender,color,temperment,alive) VALUES( NULL, 3, "F", "brown", "calm", 0);
